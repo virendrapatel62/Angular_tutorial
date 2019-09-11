eb1cd03a2386d34635f403370288f08f67af3069
Virendra <patelvirendra62@gmail.com>
2019-09-11 08:23:53 +0530

    Generating Services Using CLI

---
+++ src/app/email.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+
+// created using cli 
+//ng g s email 
+@Injectable({
+  providedIn: 'root'
+})
+export class EmailService {
+
+  constructor() { }
+}
