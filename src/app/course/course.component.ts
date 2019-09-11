bd75136aeba8ce7f0e5a4ce1171aabf8beab6e37
Virendra <patelvirendra62@gmail.com>
2019-09-11 11:50:04 +0530

    assignment 2 Faviorate

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
 @Component({
 	selector: 'course',
 	// create a pipe and regsiter in module...
 	template: `
-		{{blog | summary : 100}}
+		<span [class.far]='active' [class.fas]='!active' class='fa-star' (click)='onStarClick()' ></span>
 	`,
 })
 export class CourseComponent {
-	blog = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris in aliquam sem fringilla ut morbi tincidunt augue. A scelerisque purus semper eget duis at tellus at urna. Odio ut enim blandit volutpat maecenas. Interdum velit laoreet id donec ultrices tincidunt arcu non sodales.`
+	active = true;
+	onStarClick(){
+		this.active = !this.active
+	}
 }
