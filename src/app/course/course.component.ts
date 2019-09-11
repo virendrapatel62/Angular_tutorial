6134eabdef1d94bcac0445809c2733f3892c48dc
Virendra <patelvirendra62@gmail.com>
2019-09-11 08:13:48 +0530

    Services

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -5,11 +5,14 @@ import { Component, OnInit } from '@angular/core';
   template: `
     <h1>Title : {{ title }}</h1>
     <ul>
-      <li *ngFor='let course of courses'>{{course}}</li>
+      <li *ngFor='let course of courses'>
+        {{course}}
+      </li>
     </ul>
   `,
 })
 export class CourseComponent{
     title = "List of Course"
-    courses = ["Course 1" , "Course 2" , "Course 3"]
+    courses;
+    // login for fetching Courses from http Service 
 }
---
+++ src/app/course/courses.service.ts
@@ -0,0 +1,13 @@
+
+export class CoursesService {
+    getCourses() {
+        //..
+        return [
+            "Course 1",
+            "Course 2",
+            "Course 3",
+            "Course 4",
+            "Course 5",
+        ]
+    }
+}
