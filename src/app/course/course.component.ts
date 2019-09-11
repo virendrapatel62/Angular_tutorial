d836bb0959e79fd4ce6da916f675f5c09d50b8a5
Virendra <patelvirendra62@gmail.com>
2019-09-11 09:08:49 +0530

    adding Bootstrap

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -4,25 +4,9 @@ import { CoursesService } from './courses.service';
 @Component({
   selector: 'course',
   template: `
-      <img [src]='imageUrl'>
-      <table>
-          <tr>
-            <!-- <td [colspan]='colSpan'>Some Text</td>  no dome element like colspan error-->
-
-            <td [attr.colspan]='colSpan'>Some Text</td>
-            <td>Some Text</td>
-            <td [attr.colspan]='colSpan'>Some Text</td>
-          </tr>
-          <tr>
-            <!-- <td [colspan]='colSpan'>Some Text</td>  no dome element like colspan error-->
-
-            <td>Some Text</td>
-            <td>Some Text</td>
-          </tr>
-      </table>
+        <button class='btn btn-info' [class.disabled]='isActive' >Click Me</button>      
     `,
 })
 export class CourseComponent {
-  imageUrl = 'http://lorempixel.com/300/200/sports/2/'
-  colSpan = 2;
+	isActive = true
 }
