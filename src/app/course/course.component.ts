e99d8be3d5485cc00c6eecb3a8d48bb8b13d232c
Virendra <patelvirendra62@gmail.com>
2019-09-11 12:20:12 +0530

    assignment - title case

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -2,14 +2,21 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
 	selector: 'course',
-	// create a pipe and regsiter in module...
-	template: `
-		<span [class.far]='active' [class.fas]='!active' class='fa-star' (click)='onStarClick()' ></span>
-	`,
+	templateUrl : './course.component.html'
 })
 export class CourseComponent {
-	active = true;
-	onStarClick(){
-		this.active = !this.active
+	text = 'some email'
+	onKeyPress(value:string){
+		if(!value) return
+		this.text = ''
+		value = value.toLowerCase()
+		value = value.trim();
+		let arr = value.split(' ')
+		for(let i in arr){
+			let s = arr[i];
+			s = s.replace(s[0] , s[0].toUpperCase())+" "
+			this.text += s
+		}
+		this.text = this.text.replace(/Of The/g , 'of the')
 	}
 }
