ac547af7a29bc2b92f748c3deac43997c4755fc8
Virendra <patelvirendra62@gmail.com>
2019-09-11 08:08:52 +0530

    Directive ngFor

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'course',
   template: `
-    <h1>Title : {{ getTitle() }}</h1>
+    <h1>Title : {{ title }}</h1>
+    <ul>
+      <li *ngFor='let course of courses'>{{course}}</li>
+    </ul>
   `,
 })
 export class CourseComponent{
-    title = "List of Course using Method"
-
-    getTitle(){
-      return this.title;
-    }
+    title = "List of Course"
+    courses = ["Course 1" , "Course 2" , "Course 3"]
 }
