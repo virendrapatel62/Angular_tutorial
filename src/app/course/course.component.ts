4b59a8d38ae0a32881fd01818ab0b17ad8482e29
Virendra <patelvirendra62@gmail.com>
2019-09-11 10:28:11 +0530

    Template Variables

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,23 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { CoursesService } from './courses.service';
 
+// @Component({
+//   selector: 'course',
+//   template: `
+// 			<input type='text' 
+// 			class='form-control' 
+// 			(keyup.enter)='onEnterKeyPress($event);'>
+// 		`,
+// })
+// export class CourseComponent {
+// 	onEnterKeyPress($event){
+// 		console.log($event.target.value);
+// 	}
+// }
+
+
+// templateting
+
 @Component({
-  selector: 'course',
-  template: `
-			<input type='text' class='form-control' (keyup)='onKeyPress($event);'>
-			<input type='text' class='form-control' (keyup.enter)='onEnterKeyPress($event);'>
+	selector: 'course',
+	template: `
+			<input #email 
+			(keyup.enter)='onEnterKeyPress(email.value);'>
 		`,
 })
 export class CourseComponent {
-	color = true;
-
-	onKeyPress($event){
-		($event.keyCode == 13) ? console.log("Enter Key is Pressed..")
-		 : ''			
-	}
-
-	onEnterKeyPress($event){
-		console.log('ENter KEy Is Pressed ... Event Filter....');
-		
+	onEnterKeyPress(value) {
+		console.log(value);
 	}
 }
