362bb45b928f822290341641da7856b4a0a1b2b9
Virendra <patelvirendra62@gmail.com>
2019-09-11 08:48:45 +0530

    Diplaying Data  - bindin

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -5,26 +5,13 @@ import { CoursesService } from './courses.service';
   selector: 'course',
   template: `
     <h1>Title : {{ title }}</h1>
-    <ul>
-      <li *ngFor='let course of courses'>
-        {{course}}
-      </li>
-    </ul>
-  `,
-})
-export class CourseComponent{
-    title = "List of Course"
-    courses;
-    // login for fetching Courses from http Service 
 
-    // dependency
-    // angular will provide this dependecny called dependency injection 
-    // register dependency in modulea pp 
-    constructor(service : CoursesService){
-      // by calling constructer we tightly coupled the class with this 
-      // problem is while testing //
-      // when we want to send parement we cave to change this line 
-      // let service = new CoursesService();
-      this.courses = service.getCourses();
-    }
+    <h1 [textContent]='title'></h1>
+    <img src={{imageUrl}}>
+    <img [src]='title'>
+    `,
+})
+export class CourseComponent {
+  title = "Hello Title"
+  imageUrl = 'http://lorempixel.com/300/200/sports/2/'
 }
