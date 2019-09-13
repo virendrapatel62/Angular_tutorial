6a4949cab4afcf80f32723ef0f5248b84ced49de
Virendra <patelvirendra62@gmail.com>
2019-09-13 20:13:21 +0530

    problem with simple Asycronous Validator

--- src/app/signup-form/username.validators.ts
+++ src/app/signup-form/username.validators.ts
@@ -1,12 +1,26 @@
 import { AbstractControl, ValidationErrors } from '@angular/forms';
-
-
-export class UsernameValidators{
-    static cannotContainSpace(control : AbstractControl) : ValidationErrors | null {
-        if((<string>control.value).indexOf(' ') >= 0){
-            return { cannotContainSpace : 'true'}
-        }else{
+export class UsernameValidators {
+    static cannotContainSpace(control: AbstractControl): ValidationErrors | null {
+        if ((<string>control.value).indexOf(' ') >= 0) {
+            return { cannotContainSpace: 'true' }
+        } else {
             return null;
         }
+    }
+
+
+    static shouldBeUnique(control: AbstractControl): ValidationErrors | null {
+        // asychronous Request 
+        //....
+        setTimeout(() => {
+            console.log('sending request to server ==== ok');
+
+            if (control.value == 'Virendra') {
+                return { shouldBeUnique: true };
+            }
+        }, 2000)
+        return null;
+
+        // it always return null this is the problem so next example will solve it 
     }
 }
